68e70cca68908be0654cce6be2aeb66da209ddf6
Eduardo5011 <eduardo.rosado5011@gmail.com>
2026-07-31 17:03:11 -0400

    updated

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -931,3 +931,39 @@
 // printName(student);
 // printName(product);
 
+// FETCH DATA AND DECLARATION
+
+import {z} from 'zod';
+
+const url = "https://www.course-api.com/react-tours-project";
+
+type Tour = {
+    id: string;
+    name: string;
+    info: string;
+    image: string;
+    price: string;
+}
+
+async function fetchData(url: string): Promise<Tour[]> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: Tour[] = await response.json();
+    
+    return data;
+  } catch (error) {
+    const errorMsg =
+      error instanceof Error ? error.message : "there was an error";
+      console.error(errorMsg);
+      return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour)=> {
+    console.log(tour.name);
+})
+
